b7540cd0cfdf916ef4d61f5eb661f4be792585b8
RakalendraWildan123 <wildanrakalendra@gmail.com>
2026-09-20 20:46:52 +0700

    Update requests

--- backend/app/Http/Requests/User/StoreUserRequest.php
+++ backend/app/Http/Requests/User/StoreUserRequest.php
@@ -1,14 +1,13 @@
 <?php
 
 namespace App\Http\Requests\User;
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule\Password;
-use Illuminate\Validation\Rule;
 
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\Rule;
 
 class StoreUserRequest extends FormRequest
 {
-
     public function authorize(): bool
     {
         return true;
@@ -18,14 +17,43 @@ class StoreUserRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users', 'email')
+
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique('users', 'email'),
             ],
-            'password' => ['required', 'string', Password::min(8)->letters()->numbers()],
-            'role' => ['required', Rule::in(['admin', 'petugas', 'peminjam'])
+
+            'password' => [
+                'required',
+                'string',
+                Password::min(8)->letters()->numbers(),
             ],
-            'no_hp' => ['nullable', 'string', 'max:15'],
-            'alamat' => ['nullable', 'string'],
-            'foto_profile' => ['nullable', 'image', 'mimes:jpeg,png,jpg', 'max:2048'],
+
+            'role' => [
+                'required',
+                Rule::in(['admin', 'petugas', 'peminjam']),
+            ],
+
+            'no_hp' => [
+                'nullable',
+                'string',
+                'max:15',
+            ],
+
+            'alamat' => [
+                'nullable',
+                'string',
+            ],
+
+            'foto_profile' => [
+                'nullable',
+                'image',
+                'mimes:jpeg,png,jpg',
+                'max:2048',
+            ],
         ];
     }
 }
